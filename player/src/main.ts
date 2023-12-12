ac47cf05fe5f2f96c85b59e470c3dc6230eebc7b
Asa <26807394+a01sa01to@users.noreply.github.com>
2023-12-12 17:58:24 +0900

    feat: ts 導入

--- player/src/main.ts
+++ player/src/main.ts
@@ -4,8 +4,8 @@
  * ALGORI Javascript版 デモプレイヤー
  */
 
-const socketIoClient = require('socket.io-client');
-const BlueBird = require('bluebird');
+import socketIoClient from "socket.io-client";
+import BlueBird from "bluebird";
 
 /**
  * 定数
@@ -13,71 +13,71 @@ const BlueBird = require('bluebird');
 // Socket通信の全イベント名
 const SocketConst = {
   EMIT: {
-    JOIN_ROOM: 'join-room', // 試合参加
-    RECEIVER_CARD: 'receiver-card', // カードの配布
-    FIRST_PLAYER: 'first-player', // 対戦開始
-    COLOR_OF_WILD: 'color-of-wild', // 場札の色を変更する
-    UPDATE_COLOR: 'update-color', // 場札の色が変更された
-    SHUFFLE_WILD: 'shuffle-wild', // シャッフルしたカードの配布
-    NEXT_PLAYER: 'next-player', // 自分の手番
-    PLAY_CARD: 'play-card', // カードを出す
-    DRAW_CARD: 'draw-card', // カードを山札から引く
-    PLAY_DRAW_CARD: 'play-draw-card', // 山札から引いたカードを出す
-    CHALLENGE: 'challenge', // チャレンジ
-    PUBLIC_CARD: 'public-card', // 手札の公開
-    POINTED_NOT_SAY_UNO: 'pointed-not-say-uno', // UNO宣言漏れの指摘
-    SPECIAL_LOGIC: 'special-logic', // スペシャルロジック
-    FINISH_TURN: 'finish-turn', // 対戦終了
-    FINISH_GAME: 'finish-game', // 試合終了
-    PENALTY: 'penalty', // ペナルティ
+    JOIN_ROOM: "join-room", // 試合参加
+    RECEIVER_CARD: "receiver-card", // カードの配布
+    FIRST_PLAYER: "first-player", // 対戦開始
+    COLOR_OF_WILD: "color-of-wild", // 場札の色を変更する
+    UPDATE_COLOR: "update-color", // 場札の色が変更された
+    SHUFFLE_WILD: "shuffle-wild", // シャッフルしたカードの配布
+    NEXT_PLAYER: "next-player", // 自分の手番
+    PLAY_CARD: "play-card", // カードを出す
+    DRAW_CARD: "draw-card", // カードを山札から引く
+    PLAY_DRAW_CARD: "play-draw-card", // 山札から引いたカードを出す
+    CHALLENGE: "challenge", // チャレンジ
+    PUBLIC_CARD: "public-card", // 手札の公開
+    POINTED_NOT_SAY_UNO: "pointed-not-say-uno", // UNO宣言漏れの指摘
+    SPECIAL_LOGIC: "special-logic", // スペシャルロジック
+    FINISH_TURN: "finish-turn", // 対戦終了
+    FINISH_GAME: "finish-game", // 試合終了
+    PENALTY: "penalty", // ペナルティ
   },
 };
 
 // UNOのカードの色
 const Color = {
-  RED: 'red', // 赤
-  YELLOW: 'yellow', // 黄
-  GREEN: 'green', // 緑
-  BLUE: 'blue', // 青
-  BLACK: 'black', // 黒
-  WHITE: 'white', // 白
+  RED: "red", // 赤
+  YELLOW: "yellow", // 黄
+  GREEN: "green", // 緑
+  BLUE: "blue", // 青
+  BLACK: "black", // 黒
+  WHITE: "white", // 白
 };
 
 // UNOの記号カード種類
 const Special = {
-  SKIP: 'skip', // スキップ
-  REVERSE: 'reverse', // リバース
-  DRAW_2: 'draw_2', // ドロー2
-  WILD: 'wild', // ワイルド
-  WILD_DRAW_4: 'wild_draw_4', // ワイルドドロー4
-  WILD_SHUFFLE: 'wild_shuffle', // シャッフルワイルド
-  WHITE_WILD: 'white_wild', // 白いワイルド
+  SKIP: "skip", // スキップ
+  REVERSE: "reverse", // リバース
+  DRAW_2: "draw_2", // ドロー2
+  WILD: "wild", // ワイルド
+  WILD_DRAW_4: "wild_draw_4", // ワイルドドロー4
+  WILD_SHUFFLE: "wild_shuffle", // シャッフルワイルド
+  WHITE_WILD: "white_wild", // 白いワイルド
 };
 
 // カードを引く理由
 const DrawReason = {
-  DRAW_2: 'draw_2', // 直前のプレイヤーがドロー2を出した場合
-  WILD_DRAW_4: 'wild_draw_4', // 直前のプレイヤーがワイルドドロー4を出した場合
-  BIND_2: 'bind_2', // 直前のプレイヤーが白いワイルド（バインド2）を出した場合
-  SKIP_BIND_2: 'skip_bind_2', // 直前のプレイヤーが白いワイルド（スキップバインド2）を出した場合
-  NOTHING: 'nothing', // 理由なし
+  DRAW_2: "draw_2", // 直前のプレイヤーがドロー2を出した場合
+  WILD_DRAW_4: "wild_draw_4", // 直前のプレイヤーがワイルドドロー4を出した場合
+  BIND_2: "bind_2", // 直前のプレイヤーが白いワイルド（バインド2）を出した場合
+  SKIP_BIND_2: "skip_bind_2", // 直前のプレイヤーが白いワイルド（スキップバインド2）を出した場合
+  NOTHING: "nothing", // 理由なし
 };
 
-const TEST_TOOL_HOST_PORT = '3000'; // 開発ガイドラインツールのポート番号
+const TEST_TOOL_HOST_PORT = "3000"; // 開発ガイドラインツールのポート番号
 const ARR_COLOR = [Color.RED, Color.YELLOW, Color.BLUE, Color.GREEN]; // 色変更の選択肢
 
 /**
  * コマンドラインから受け取った変数等
  */
-const host = process.argv[2] || ''; // 接続先（ディーラープログラム or 開発ガイドラインツール）
-const roomName = process.argv[3] || ''; // ディーラー名
-const player = process.argv[4] || ''; // プレイヤー名
+const host = process.argv[2] || ""; // 接続先（ディーラープログラム or 開発ガイドラインツール）
+const roomName = process.argv[3] || ""; // ディーラー名
+const player = process.argv[4] || ""; // プレイヤー名
 const eventName = process.argv[5]; // Socket通信イベント名
 const isTestTool = host.includes(TEST_TOOL_HOST_PORT); // 接続先が開発ガイドラインツールであるかを判定
-const SPECIAL_LOGIC_TITLE = '◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯'; // スペシャルロジック名
+const SPECIAL_LOGIC_TITLE = "◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯◯"; // スペシャルロジック名
 const TIME_DELAY = 10; // 処理停止時間
 
-let id = ''; // 自分のID
+let id = ""; // 自分のID
 let unoDeclared = {}; // 他のプレイヤーのUNO宣言状況
 
 /**
@@ -85,7 +85,7 @@ let unoDeclared = {}; // 他のプレイヤーのUNO宣言状況
  */
 if (!host) {
   // 接続先のhostが指定されていない場合はプロセスを終了する
-  console.log('Host missed');
+  console.log("Host missed");
   process.exit();
 } else {
   console.log(`Host: ${host}`);
@@ -93,7 +93,7 @@ if (!host) {
 
 // ディーラー名とプレイヤー名の指定があることをチェックする
 if (!roomName || !player) {
-  console.log('Arguments invalid');
+  console.log("Arguments invalid");
   if (!isTestTool) {
     // 接続先がディーラープログラムの場合はプロセスを終了する
     process.exit();
@@ -109,24 +109,24 @@ const TEST_TOOL_EVENT_DATA = {
     room_name: roomName,
   },
   [SocketConst.EMIT.COLOR_OF_WILD]: {
-    color_of_wild: 'red',
+    color_of_wild: "red",
   },
   [SocketConst.EMIT.PLAY_CARD]: {
-    card_play: { color: 'black', special: 'wild' },
+    card_play: { color: "black", special: "wild" },
     yell_uno: false,
-    color_of_wild: 'blue',
+    color_of_wild: "blue",
   },
   [SocketConst.EMIT.DRAW_CARD]: {},
   [SocketConst.EMIT.PLAY_DRAW_CARD]: {
     is_play_card: true,
     yell_uno: true,
-    color_of_wild: 'blue',
+    color_of_wild: "blue",
   },
   [SocketConst.EMIT.CHALLENGE]: {
     is_challenge: true,
   },
   [SocketConst.EMIT.POINTED_NOT_SAY_UNO]: {
-    target: 'Player 1',
+    target: "Player 1",
   },
   [SocketConst.EMIT.SPECIAL_LOGIC]: {
     title: SPECIAL_LOGIC_TITLE,
@@ -135,7 +135,7 @@ const TEST_TOOL_EVENT_DATA = {
 
 // Socketクライアント
 const client = socketIoClient.connect(host, {
-  transports: ['websocket'],
+  transports: ["websocket"],
 });
 
 /**
@@ -289,7 +289,7 @@ if (isTestTool) {
   // テストツールに接続
   if (!eventName) {
     // イベント名の指定がない（開発ガイドラインSTEP2の受信のテストを行う時）
-    console.log('Not found event name.');
+    console.log("Not found event name.");
   } else if (!TEST_TOOL_EVENT_DATA[eventName]) {
     // イベント名の指定があり、テストデータが定義されていない場合はエラー
     console.log(`Undefined test data. eventName: ${eventName}`);
@@ -315,15 +315,15 @@ if (isTestTool) {
 /**
  * Socket通信の確立
  */
-client.on('connect', () => {
-  console.log('Client connect successfully!');
+client.on("connect", () => {
+  console.log("Client connect successfully!");
 });
 
 /**
  * Socket通信を切断
  */
-client.on('disconnect', (dataRes) => {
-  console.log('Client disconnect.');
+client.on("disconnect", (dataRes) => {
+  console.log("Client disconnect.");
   console.log(dataRes);
   // プロセスを終了する
   process.exit();
@@ -419,7 +419,10 @@ client.on(SocketConst.EMIT.NEXT_PLAYER, (dataRes) => {
       };
 
       // 出すカードがワイルドとワイルドドロー4の時は変更する色を指定する
-      if (playCard.special === Special.WILD || playCard.special === Special.WILD_DRAW_4) {
+      if (
+        playCard.special === Special.WILD ||
+        playCard.special === Special.WILD_DRAW_4
+      ) {
         const color = selectChangeColor(); // 指定する色
         data.color_of_wild = color;
       }
@@ -444,7 +447,10 @@ client.on(SocketConst.EMIT.NEXT_PLAYER, (dataRes) => {
 
         const playCard = res.draw_card[0]; // 引いたカード。draw-cardイベントのcallbackデータは引いたカードのリスト形式であるため、配列の先頭を指定する。
         // 引いたカードがワイルドとワイルドドロー4の時は変更する色を指定する
-        if (playCard.special === Special.WILD || playCard.special === Special.WILD_DRAW_4) {
+        if (
+          playCard.special === Special.WILD ||
+          playCard.special === Special.WILD_DRAW_4
+        ) {
           const color = selectChangeColor();
           data.color_of_wild = color;
         }
