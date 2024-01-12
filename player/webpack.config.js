04d08b3d0443819d7aaccefe40c72ec8f6b73eae
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-01-12 12:47:50 +0900

    socket つながらなかった、あぶね～

--- player/webpack.config.js
+++ player/webpack.config.js
@@ -2,26 +2,25 @@ const path = require('path');
 
 module.exports = {
   entry: './src/main.ts',
+  target: "node",
   output: {
     path: path.resolve(__dirname),
     filename: 'player.js',
   },
+  externals: {
+    bufferutil: "bufferutil",
+    "utf-8-validate": "utf-8-validate",
+  },
   module: {
     rules: [
       {
-        // 拡張子 .ts の場合
         test: /\.ts$/,
-        // TypeScript をコンパイルする
         use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
-  // import 文で .ts ファイルを解決するため
-  // これを定義しないと import 文で拡張子を書く必要が生まれる。
-  // フロントエンドの開発では拡張子を省略することが多いので、
-  // 記載したほうがトラブルに巻き込まれにくい。
   resolve: {
-    // 拡張子を配列で指定
     extensions: [
       '.ts', '.js',
     ],
